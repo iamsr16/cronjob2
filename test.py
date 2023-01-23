3af74e1775fd9c1bb6422dcbc702904702cf0f15
iamsr16 <83810443+iamsr16@users.noreply.github.com>
2023-01-23 18:13:40 +0530

    Update and rename test.py to hello.py

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-# python script which needs an environment variable and runs as a cron job
-import datetime
-import os
-
-test_environ = os.environ["TEST_ENV"]
-print "Cron job has run at %s with environment variable '%s'" %(datetime.datetime.now(), test_environ)
